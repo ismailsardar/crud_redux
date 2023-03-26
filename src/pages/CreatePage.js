334403ef725d15c00013da37f0f65b09b80cb7c9
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-03-26 08:59:30 +0600

    two page created

---
+++ src/pages/CreatePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CreateUpdateForm from '../Components/CreateUpdateForm'
+
+const CreatePage = () => {
+  return (
+    <>
+        <CreateUpdateForm/>
+    </>
+  )
+}
+
+export default CreatePage
---
+++ src/pages/ReadPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductList from '../Components/ProductList'
+
+const ReadPage = () => {
+  return (
+    <>
+        <ProductList/>
+    </>
+  )
+}
+
+export default ReadPage
